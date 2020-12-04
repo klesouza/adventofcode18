8d703d996b5b3b77d0a7546d0b803f6c148bde77
klesouza <kle_zoio@hotmail.com>
2020-12-05 00:06:15 +0100

    day1 part2

--- 2020/day01.fsx
+++ 2020/day01.fsx
@@ -7,18 +7,12 @@ type ResultSum =
     | Found of int
 
 
-let rec getSum2 (arr: list<int>) ie (i: int) =
+let rec getSum (arr: list<int>) ie (i: int) =
     // printfn "%d %d" ie i
     let s = i + arr.[ie]
-    if s > 2020 then getSum2 arr (ie - 1) i
+    if s > 2020 then getSum arr (ie - 1) i
     elif s = 2020 then Found (i * arr.[ie])
     else NotYet (arr, ie)
-
-let getSum3 result =
-    // printfn "%A" result
-    match result with
-        | NotYet _ -> None
-        | Found v -> Some v
 
 let day01 (arr: seq<string>) = 
     let filter2020 =
@@ -30,8 +24,35 @@ let day01 (arr: seq<string>) =
     let day01Array = Seq.toList filter2020
      
     filter2020
-        |> Seq.map ((getSum2) day01Array (day01Array.Length-1))
-        |> Seq.pick getSum3
+        |> Seq.map ((getSum) day01Array (day01Array.Length-1))
+        |> Seq.pick (fun result ->
+            match result with
+                | NotYet _ -> None
+                | Found v -> Some v
+        )
+
+let day01p2 (arr: seq<string>)  =
+    let filter2020 =
+        arr
+        |> Seq.map int
+        |> Seq.sort
+        |> Seq.filter ((>) 2020)
+
+    let day01Array = Seq.toList filter2020
+    
+    //Give up trying to make it more functional :|
+    let mutable found = -1
+    let mutable x = 0
+    let mutable y = 1
+    let mutable z = (day01Array.Length-1)
+    while found = -1 do
+        let s = day01Array.[x] + day01Array.[y] + day01Array.[z]
+        if s > 2020 then z <- z - 1
+        elif s < 2020 then
+            if y = (z - 1) then x <- x+1
+            else y <- y+1
+        else found <- day01Array.[x] * day01Array.[y] * day01Array.[z]
+    found
 
 // printfn "%i" (day01 input)
 // [ 
@@ -43,3 +64,5 @@ let day01 (arr: seq<string>) =
 // "1456" ]
 // |> day01
 // |> printfn "%i"
+
+
